81203a8c0d4714aa8264d713fff007349c3b802c
Yunaaaard <tarimanleonard28@gmail.com>
2026-03-26 14:10:13 +0800

    Initial Commit

---
+++ lib/views/LandingScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Landingscreen extends StatefulWidget {
+  const Landingscreen({super.key});
+
+  @override
+  State<Landingscreen> createState() => _LandingscreenState();
+}
+
+class _LandingscreenState extends State<Landingscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
