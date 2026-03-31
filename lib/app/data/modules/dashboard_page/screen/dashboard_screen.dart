8c643a45bce0fb8823db1d62a45925b986e758fc
Yunaaaard <tarimanleonard28@gmail.com>
2026-03-31 10:36:36 +0800

    fix again structure

--- lib/app/data/modules/dashboard_page/screen/dashboard_screen.dart
+++ lib/app/data/modules/dashboard_page/screen/dashboard_screen.dart
@@ -1,12 +1,10 @@
-
 import 'dart:async';
-
 import 'package:fast_sosyo/app/data/modules/check_eligibility/check_eligibility_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import '../models/brand_category.dart';
 import '../models/dashboard_stats.dart';
-import '../controllers/data_controller.dart';
+import '../controllers/dashboard_controller.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
